7d367d12331a2c3d9d76cfa309c28dcfe02a22d8
Tobias Noelkensmeier <tobiassherco125@gmail.com>
2022-04-03 11:54:19 +0200

    Level 2 working

--- game raycast - Copy (2)/Assets/Scripts/LevelLoader.cs
+++ game raycast - Copy (2)/Assets/Scripts/LevelLoader.cs
@@ -12,9 +12,7 @@ public class LevelLoader : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*if (Input.GetMouseButtonDown(0)) {
-            NextLevel();
-        }*/
+        
     }
     
 
